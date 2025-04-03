123f05fde618c76da2fb759cd0c6e79f7d33f300
Surya kranthi kiran <tskranthikiran@gmail.com>
2025-04-03 17:02:37 +0530

    Add new routes and change server port to 8005

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express = require('express');
 const app = express();
 
 
-app.use("/", (req, res) => {
-    res.send("Hello Surya welocme to express js")
+
+
+app.get("/user", (req, res) => {
+    // res.send("Hello Surya welcome to home page")
+    res.send({ firstName: "Surya", lastName: "Kumar" })
 })
+
+app.post("/user", (req, res) => {
+    res.send("Saved data to the DB successfully")
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted data from the DB successfully")
+})
+
 
 app.use("/info", (req, res) => {
     res.send("Hello Surya welocme to info page")
 })
 
-app.use("/user", (req, res) => {
-    res.send("Hello Surya welocme to users page")
-})
+
 
 app.listen(8005, () => {
     console.log('Server is running on port 8005');
