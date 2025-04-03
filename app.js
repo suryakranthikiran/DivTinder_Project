92be8abdb1b28c2687cbe1cd9e732b95559d8ee0
Tamarapilli <s.tamarapilli2@dxc.com>
2025-04-03 15:05:00 +0530

    added routes

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+app.use("/", (req, res) => {
+    res.send("Hello Surya welocme to express js")
+})
+
+app.use("/info", (req, res) => {
+    res.send("Hello Surya welocme to info page")
+})
+
+app.use("/user", (req, res) => {
+    res.send("Hello Surya welocme to users page")
+})
+
+app.listen(8005, () => {
+    console.log('Server is running on port 8005');
 });
