a4e8b35b014be8b68068ff747e5cdfbf6be53232
Surya kranthi kiran <tskranthikiran@gmail.com>
2025-04-04 23:28:40 +0530

    Refactor user update endpoint to use URL parameter for user ID; add validation for allowed updates and skills limit

--- src/index.js
+++ src/index.js
@@ -64,11 +64,20 @@ app.delete("/user", async (req, res) => {
 
 // Update the data of the user 
 
-app.patch("/user", async (req, res) => {
-
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;
     const data = req.body;
     try {
+        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((key) => {
+            return ALLOWED_UPDATES.includes(key)
+        })
+        if (!isUpdateAllowed) {
+            throw new Error("Invalid updates")
+        }
+        if (data.skills.length > 10) {
+            throw new Error("Skills should be less than 10")
+        }
         const user = await User.findByIdAndUpdate(userId, data, { returnDocument: 'after', runValidators: true });
         // console.log(user);
         if (!user) {
@@ -77,9 +86,8 @@ app.patch("/user", async (req, res) => {
             res.send("User updated successfully");
         }
     } catch (error) {
-        res.status(500).send("Error updating user" + error.message)
+        res.status(500).send("Error updating " + error.message)
     }
-
 })
 
 connectDB().then(() => {
