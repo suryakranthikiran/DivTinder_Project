ce586fdf4fdd34f99db8894ba1dcd064fad138e3
Surya kranthi kiran <tskranthikiran@gmail.com>
2025-04-06 11:26:59 +0530

    fix: replace bcryptjs with bcrypt for password hashing

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     firstName: {
