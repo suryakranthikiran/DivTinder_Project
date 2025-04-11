af8fab84c28c8044f6418f47660bd5b86e28699e
Surya kranthi kiran <tskranthikiran@gmail.com>
2025-04-11 22:54:58 +0530

    feat: add endpoints for user connections and feed, enhancing user visibility and filtering

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const connectionRequestModel = require("../models/connectionRequest");
+const { set } = require("mongoose");
+const user = require("../models/user");
 const userRouter = express.Router();
 // Get all the pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -20,5 +22,72 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     }
 })
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user
+        const connectionRequest = await connectionRequestModel.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
+            ]
+        }).populate("fromUserId", "firstName lastName photoUrl gender age about skills").populate("toUserId", "firstName lastName photoUrl gender age about skills")
+        const data = connectionRequest.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId
+            }
+            return row.fromUserId
+        })
+        res.json({ data })
+    } catch (error) {
+        res.status(404).send("ERROR : ", error.message)
+    }
+})
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+
+    try {
+
+        // user should see all the user cards except
+        // 1. his own card
+        // 2. his connections
+        // 3. ignored people
+        // 4. already sent connection request
+
+        // example : Rahul=>[Surya,sarojini,Srinu,Jesus,Lakshmi,Tarun]
+
+        // Rahul->Surya Rahul->Sarojini(liked both surya and sarojini) now rahul feed can see => [Srinu,Jesus,Lakshmi,Tarun]
+
+        // Surya rejected Rahul request so Rahul can't see surya profile again
+
+        // Sarojini accepted the Rahul request so Sarojini profile can't display in Rahul Feed
+
+        const loggedInUser = req.user;
+
+        // find all connectionRequest either i have sent or i received
+
+        const connectionRequest = await connectionRequestModel.find({
+            $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }]
+        }).select("fromUserId toUserId")
+
+        const hideUsersFromFeed = new Set()
+
+        connectionRequest.forEach(req => {
+            hideUsersFromFeed.add(req.fromUserId.toString())
+            hideUsersFromFeed.add(req.toUserId.toString())
+        })
+
+        const users = await user.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select("firstName lastName photoUrl gender age about skills")
+        res.send(users)
+
+    } catch (error) {
+        res.status(404).send("ERROR : ", error.message)
+    }
+
+})
 
 module.exports = userRouter;
